951633f275a179e683adbe490f136428bd581a34
loojee <mynameislujie1992@gmail.com>
2024-03-23 22:26:48 +0800

    fix: fix retry condition

--- client.go
+++ client.go
@@ -51,7 +51,7 @@ func New(corpID string, opts ...CtorOption) *WorkWX {
 	optionsObj.restyCli.SetRetryWaitTime(100 * time.Millisecond)
 	optionsObj.restyCli.SetRetryMaxWaitTime(200 * time.Millisecond)
 	optionsObj.restyCli.AddRetryCondition(func(response *resty.Response, err error) bool {
-		if err != nil || response.StatusCode() > http.StatusInternalServerError {
+		if err != nil || response.StatusCode() >= http.StatusInternalServerError {
 			return true
 		}
 
